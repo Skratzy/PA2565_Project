fedf32a6828d85ca749d7f5cfbcb0b131d31f3f8
Skratzy <ettafredrik@gmail.com>
2018-12-17 15:13:57 +0100

    Added so the mdels get cleared and reloaded every second

--- PA2565_Project/ResourceManager/ResManMain.cpp
+++ PA2565_Project/ResourceManager/ResManMain.cpp
@@ -1,8 +1,3 @@
-//------------------------------------------------------------------------------
-//  texcube-d3d11.c
-//  Texture creation and rendering.
-//------------------------------------------------------------------------------
-
 #include <thread>
 #include <fstream>
 #include <sstream>
@@ -44,7 +39,7 @@ void initMemMngr() {
 	// Single frame stack
 	stackInstances.push_back(StackInstance{ megabyte }); // 1 megabyte
 	// Persistent stack
-	stackInstances.push_back(StackInstance{ megabyte }); // 1 megabyte
+	stackInstances.push_back(StackInstance{ megabyte / 512 }); // 1 megabyte
 	// Function stack
 	stackInstances.push_back(StackInstance{ megabyte }); // 1 megabyte
 
@@ -67,18 +62,18 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 	// Open a debug console window if running in debug mode
 //#ifdef _DEBUG
-	AllocConsole();
+	AllocConsole(); // Roughly 30 MB
 	FILE* a;
 	freopen_s(&a, "CONIN$", "r", stdin);
 	freopen_s(&a, "CONOUT$", "w", stdout);
 	freopen_s(&a, "CONOUT$", "w", stderr);
-	//#endif
+//#endif
 
 		/* setup d3d11 app wrapper and sokol_gfx */
 	const int msaa_samples = 4;
 	const int width = 800;
 	const int height = 600;
-	d3d11_init(width, height, msaa_samples, L"PA2565 Project");
+	d3d11_init(width, height, msaa_samples, L"PA2565 Project"); // Roughly 35MB
 
 	sg_desc sgDesc{ 0 };
 	sgDesc.d3d11_device = d3d11_device();
@@ -172,8 +167,10 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	rm.registerFormatLoader(new RMTextureLoader);
 
 	// Load the async temp mesh and texture
-	auto tempMesh = ResourceManager::getInstance().load("Assets/meshes/tempOBJRes.obj");
-	auto tempTex = ResourceManager::getInstance().load("Assets/textures/tempJPGRes.jpg");
+	std::string tempMeshPath = "Assets/meshes/tempOBJRes.obj";
+	auto tempMesh = ResourceManager::getInstance().load(tempMeshPath.c_str());
+	std::string tempTexPath = "Assets/textures/tempJPGRes.jpg";
+	auto tempTex = ResourceManager::getInstance().load(tempTexPath.c_str());
 
 	sg_draw_state drawState{ 0 };
 	drawState.pipeline = pip;
@@ -182,17 +179,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 	Transform sunDir;
 	std::vector<Model*> models;
-	/*for (int i = 0; i < 2; i++) {
-		models.push_back(RM_NEW(Model));
-		models.back()->getTransform().translate(HMM_Vec3(0.f, -3.5f + float(i) / 2.5f, -3.f - float(i) * 3.f));
-	}*/
-
-	//Model* house = RM_NEW(Model);
-	//rm.asyncLoad("Assets/teapot.obj", std::bind(&Model::setMeshNoDeref, house, std::placeholders::_1));
-	//rm.asyncLoad("Assets/testImage1.jpg", std::bind(&Model::setTexNoDeref, house, std::placeholders::_1));
-	//models.push_back(house);
-	//house->getTransform().translate(HMM_Vec3(0.f, -10.f, -20.f));
-	//house->getTransform().setScale(HMM_Vec3(10.f, 10.f, 10.f));
 
 	/*
 		Testcases
@@ -201,6 +187,8 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	/*
 	// Support for all supported formats and loading from zip folders
 	*/
+
+	auto marker = MemoryManager::getInstance().getStackMarker(PERSISTENT_STACK_INDEX);
 
 	/*
 	* OBJ
@@ -309,9 +297,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	debugMsg = std::string("Loading of testImage1.rmtex from zip took: " + std::to_string(timeTaken) + "ms.");
 	RM_DEBUG_MESSAGE(debugMsg, 0);
 
-
-
-
 	/*
 	*	Thread-safety
 	*/
@@ -368,7 +353,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 	bool keepRunning = true;
 	
-	auto sokolFunc = [&camEye, &vsParams, &camCenter, &camUp, &proj, &pass_action, &sunDir, &sunDirVec, &models, &rm, &drawState, &keepRunning]() {
+	auto sokolFunc = [&camEye, &vsParams, &camCenter, &camUp, &proj, &pass_action, &sunDir, &sunDirVec, &models, &rm, &drawState, &keepRunning, &tempMeshPath, &tempTexPath, &marker]() {
 		auto startTime = std::chrono::high_resolution_clock::now();
 		while (d3d11_process_events()) {
 
@@ -384,39 +369,39 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 			sunDir.rotateAroundX(0.3f);
 			vsParams.sunDir = HMM_MultiplyMat4ByVec4(sunDir.getMatrix(), sunDirVec);
 
-			bool switchModels = false;
-			if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - startTime) > std::chrono::milliseconds(500)) {
+			if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - startTime) > std::chrono::milliseconds(1000)) {
+				for (auto m : models)
+					m->~Model();
+				models.clear();
+				MemoryManager::getInstance().deallocateStack(PERSISTENT_STACK_INDEX, marker);
+
+				models.push_back(RM_NEW_PERSISTENT(Model));
+				models.back()->getTransform().translate(HMM_Vec3(10.f, -8.f, -20.f));
+				rm.asyncLoad("Assets/meshes/cow-nonormals.obj", std::bind(&Model::setMeshCallback, models.back(), std::placeholders::_1));
+				rm.asyncLoad("Assets/textures/testImage.png", std::bind(&Model::setTexCallback, models.back(), std::placeholders::_1));
+
+
+				models.push_back(RM_NEW_PERSISTENT(Model));
+				models.back()->getTransform().translate(HMM_Vec3(-10.f, -8.f, -20.f));
+				rm.asyncLoad("Assets/meshes/cow-nonormals.obj", std::bind(&Model::setMeshCallback, models.back(), std::placeholders::_1));
+				rm.asyncLoad("Assets/textures/testImage.png", std::bind(&Model::setTexCallback, models.back(), std::placeholders::_1));
+
+				models.push_back(RM_NEW_PERSISTENT(Model));
+				models.back()->getTransform().translate(HMM_Vec3(0.f, 0.f, -20.f));
+				rm.asyncLoad("Assets/meshes/cow-nonormals.obj", std::bind(&Model::setMeshCallback, models.back(), std::placeholders::_1));
+				rm.asyncLoad("Assets/textures/testImage.png", std::bind(&Model::setTexCallback, models.back(), std::placeholders::_1));
+
+				models.push_back(RM_NEW_PERSISTENT(Model));
+				models.back()->getTransform().translate(HMM_Vec3(0.f, -17.f, -20.f));
+				rm.asyncLoad("Assets/meshes/cow-nonormals.obj", std::bind(&Model::setMeshCallback, models.back(), std::placeholders::_1));
+				rm.asyncLoad("Assets/textures/testImage.png", std::bind(&Model::setTexCallback, models.back(), std::placeholders::_1));
+
 				startTime = std::chrono::high_resolution_clock::now();
-				switchModels = true;
 			}
-			switchModels = false;
 			float index = 0.1f;
 			for (auto model : models) {
 				//model->getTransform().rotateAroundY(5.f);
 				model->draw(drawState, vsParams);
-				if (switchModels) {
-					auto rndVal = std::rand() % 100;
-
-					if (rndVal < 10) {
-						model->setMesh(reinterpret_cast<MeshResource*>(rm.load("Assets/meshes/cow-normals.obj")));
-					}
-					else if (rndVal < 50) {
-						model->setMesh(reinterpret_cast<MeshResource*>(rm.load("Assets/meshes/teapot.obj")));
-					}
-					else {
-						model->setMesh(reinterpret_cast<MeshResource*>(rm.load("Assets/meshes/cow-normals-test.obj")));
-					}
-
-					if (rndVal < 30) {
-						model->setTexture(reinterpret_cast<TextureResource*>(rm.load("Assets/textures/testImage1.jpg")));
-					}
-					else if (rndVal < 50) {
-						model->setTexture(reinterpret_cast<TextureResource*>(rm.load("Assets/textures/testImage.png")));
-					}
-					else {
-						model->setTexture(reinterpret_cast<TextureResource*>(rm.load("Assets/textures/testFile.png")));
-					}
-				}
 			}
 
 
